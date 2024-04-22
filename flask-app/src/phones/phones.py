0b553149f5656968d416cc5d4d9cde0fb892c865
JacobAMedici <medici.j@northeastern.edu>
2024-04-21 19:25:16 -0700

    Changed Peripherals and Phones

--- flask-app/src/phones/phones.py
+++ flask-app/src/phones/phones.py
@@ -1,28 +1,32 @@
-from flask import Blueprint, request, jsonify, make_response, current_app
+from flask import Blueprint, request, jsonify, make_response, current_app, redirect
 import json
 from src import db
 
 phones = Blueprint('phones', __name__)
 
-@phones.route('/', methods=['GET'])
+@phones.route('/<userID>', methods=['GET', 'POST'])
 def phones_pages():
-    return "phones"
+    if request.method == 'GET':
+        return "Get Phones Page"
+    elif request.method == 'POST':
+        phoneID = request.form.get("phoneID")
+        return redirect(f'laptops/{userID}/{phoneID}')
 
-@phones.route('/<phone_id>', methods=['GET'])
+@phones.route('/<userID>/<phone_id>', methods=['GET'])
 def display_phone(phone_id):
     return "This is where we will display the phone of phone_id"
     '''
     SELECT * FROM Phone WHERE phoneID = <phone_id>;
     '''
 
-@phones.route('/<phone_id>', methods=['DELETE'])
+@phones.route('/<userID>/<phone_id>', methods=['DELETE'])
 def delete_phone(phone_id):
     return "This is where we will delete the phone of phone_id"
     '''
     DELETE FROM Phone WHERE phoneID = <phone_id>;
     '''
 
-@phones.route('/add', methods=['POST'])
+@phones.route('/<userID>/add', methods=['POST'])
 def add_phone():
     return "This will add a phone with the appropriate fields"
     '''
@@ -30,7 +34,7 @@ def add_phone():
     INSERT INTO MakesPhone(manufacturerID, phoneID)
     '''
 
-@phones.route('/<phone_id>', methods=['POST'])
+@phones.route('/<userID>/<phone_id>', methods=['POST'])
 def favorite_phone(phone_id):
     return "This is where we will favorite the phone of phone_id"
     '''
