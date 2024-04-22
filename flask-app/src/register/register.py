be3e09a42eebfb2814318030eecec10505b74b7a
JacobAMedici <medici.j@northeastern.edu>
2024-04-22 14:45:00 -0700

    Fixed Return Values

--- flask-app/src/register/register.py
+++ flask-app/src/register/register.py
@@ -24,30 +24,30 @@ def add_user():
             pass_len = len(request.form.get("password"))
     # Ensure firstName was submitted
     if not request.form.get("firstName"):
-        raise Exception("Must Provide First Name")
+        return make_response(jsonify(message='Must Provide First Name'), 400)
     # Ensure lastName was submitted
     if not request.form.get("lastName"):
-        raise Exception("Must Provide Last Name")
+        return make_response(jsonify(message='Must Provide Last Name'), 400)
     # Ensure email was submitted
     if not request.form.get("email"):
-        raise Exception("Must Provide Email")
+        return make_response(jsonify(message='Must Provide Email'), 400)
     # Ensure username was submitted
     if not request.form.get("username"):
-        raise Exception("Must Provide Username")
+        return make_response(jsonify(message='Must Provide Username'), 400)
     # Check if username is taken
     elif len((cursor().execute("SELECT * FROM users WHERE username = %s", (request.form.get("username"),))).fetchall()) > 0:
-        raise Exception("Username is Taken")
+        return make_response(jsonify(message='Username is Taken'), 400)
     elif c < 1 or didg < 1 or pass_len < 8:
-        raise Exception("Password Must Fit Reqirements")
+        return make_response(jsonify(message='Password Must Fit Reqirements'), 400)
     # Ensure password was submitted
     elif not request.form.get("password"):
-        raise Exception("Must Provide Password")
+        return make_response(jsonify(message='Must Provide Password'), 400)
     # Ensure confirm was submitted
     elif not request.form.get("confirmation"):
-        raise Exception("Must Confirm Password")
+        return make_response(jsonify(message='Must Confirm Password'), 400)
     # Make sure passwords match
     elif request.form.get("confirmation") != request.form.get("password"):
-        raise Exception("Passwords Must Match")
+        return make_response(jsonify(message='Passwords Must Match'), 400)
     # Put it in the data base and return to the home page
     elif request.form.get("confirmation") == request.form.get("password"):
         firstName = request.form.get("firstName")
