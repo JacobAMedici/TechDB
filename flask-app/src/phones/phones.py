8f746a6be0ba9cb1e2c16351ec81812190151cbe
Alec Borer <adborer65@gmail.com>
2024-04-22 11:53:21 -0700

    fixing phones Get

--- flask-app/src/phones/phones.py
+++ flask-app/src/phones/phones.py
@@ -4,10 +4,19 @@ from src import db
 
 phones = Blueprint('phones', __name__)
 
-@phones.route('/<userID>', methods=['POST'])
+@phones.route('/<userID>', methods=['GET'])
 def phones_pages(userID):
-    phoneID = request.form.get("phoneID")
-    return redirect(f'/phones/{userID}/{phoneID}')
+    cursor = db.get_db().cursor()
+    cursor.execute('SELECT * FROM Phone')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
 
 @phones.route('/<userID>/<phone_id>', methods=['POST'])
 def display_phone(userID, phone_id):
